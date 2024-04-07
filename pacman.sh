fd4277ae5db9f48325beb73f657c49ef579989c6
sentothecopycat <sotentothecopycat@gmail.com>
2024-04-08 00:03:37 +0100

    repo

---
+++ pacman.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo echo " 
+[custom] 
+SigLevel = Optional TrustAll
+Server = file:///home/sento/hyprthreds/Pkgs" >> /etc/pacman.conf
+
+cat /etc/pacman.conf
+
+sudo pacman -Syyu
+
+sudo pacman -S swww grimblast-git hyprdots-ctl-git hyprpicker oh-my-zsh-git wlogout swaylock-effects-git pokemon-colorscripts-git python-pyamdgpuinfo rofi-lbonn-wayland-git zsh-theme-powerlevel10k
